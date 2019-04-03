c4da30ce56564c8e8c8f1b7a56e4c2f287e169a3
Alejandro Muñoz Aranda <56033895+alemunara@users.noreply.github.com>
2019-04-03 19:35:01 +0000

    youtube classes

--- mashup/FooDreams/src/main/java/aiss/model/resources/YoutubeResource.java
+++ mashup/FooDreams/src/main/java/aiss/model/resources/YoutubeResource.java
@@ -1,7 +1,33 @@
 package aiss.model.resources;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.logging.Logger;
+
+import org.restlet.resource.ClientResource;
+
+import aiss.model.youtube.VideoSearch;
+
 public class YoutubeResource {
 	
+	private static final String YOUTUBE_API_KEY = "AIzaSyAor0IdwSo8mqy3vbaGatcqdnErzMKH3Js";
+	
+	private static final Logger log = Logger.getLogger(YoutubeResource.class.getName());
+	
+	public VideoSearch getVideos(String query) throws UnsupportedEncodingException {
+		
+		String q = URLEncoder.encode(query, "UTF-8");
+		
+		String uri = "https://www.googleapis.com/youtube/v3/search?key=" + YOUTUBE_API_KEY + 
+				"&part=snippet&q=" + q;
+		
+		ClientResource cr = new ClientResource(uri);
+		VideoSearch vs = cr.get(VideoSearch.class);
+		
+		
+		return vs;
+		
+	}
 	
 
 }
