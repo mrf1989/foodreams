3319b88db8e818f56e0fb2379169c0b841bc766f
marruafer <marruafer@597de019-76a1-4933-8ca1-2e2d750c62b9>
2019-05-01 09:07:44 +0000

    Implementación de eliminación de playlists creadas por el usuario

---
+++ mashup/FooDreams/src/main/java/aiss/controller/DeletePlaylistController.java
@@ -0,0 +1,62 @@
+package aiss.controller;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import aiss.model.resources.YoutubeResource;
+
+/**
+ * Servlet implementation class DeletePlaylistController
+ */
+public class DeletePlaylistController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private static final Logger log = Logger.getLogger(DeletePlaylistController.class.getName());   
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeletePlaylistController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String accessToken = (String) request.getSession().getAttribute("Youtube-token");
+		String playlistId = request.getParameter("playlistId");
+		
+		if (accessToken != null && !"".equals(accessToken)) {
+			YoutubeResource ytResource = new YoutubeResource(accessToken);
+			log.log(Level.INFO, "Tratando de eliminar la playlist: " + playlistId);
+			boolean deletePlaylist = ytResource.deletePlaylist(playlistId);
+			
+			if (deletePlaylist) {
+				log.log(Level.FINE, "Playlist eliminada con éxito.");
+				request.getRequestDispatcher("/youtubePlaylists").forward(request, response);
+			} else {
+				log.log(Level.WARNING, "No se pudo eliminar la playlist.");
+				request.getRequestDispatcher("/").forward(request, response);
+			}
+			
+		} else {
+			log.info("Intentando acceder al servicio de playlists de Youtube sin accessToken. Redirigiendo al OAuth servlet");
+			request.getRequestDispatcher("/AuthController/Youtube").forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
