cfc9cbb347102ebbce19014b7ec67f89ee470b82
Juan Carlos Cortés Muñoz <56033673+juacormun@users.noreply.github.com>
2019-05-03 19:57:20 +0000

    El commit del cucurucho de Miguel Toro, viva la regla Java!!!

--- mashup/FooDreams/src/main/java/aiss/model/foodreams/Ingrediente.java
+++ mashup/FooDreams/src/main/java/aiss/model/foodreams/Ingrediente.java
@@ -69,4 +69,54 @@ public class Ingrediente {
 		this.calorias = calorias;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((calorias == null) ? 0 : calorias.hashCode());
+		result = prime * result + ((cantidad == null) ? 0 : cantidad.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((unidad == null) ? 0 : unidad.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ingrediente other = (Ingrediente) obj;
+		if (calorias == null) {
+			if (other.calorias != null)
+				return false;
+		} else if (!calorias.equals(other.calorias))
+			return false;
+		if (cantidad == null) {
+			if (other.cantidad != null)
+				return false;
+		} else if (!cantidad.equals(other.cantidad))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (unidad == null) {
+			if (other.unidad != null)
+				return false;
+		} else if (!unidad.equals(other.unidad))
+			return false;
+		return true;
+	}
+
+	
 }
